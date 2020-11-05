da6582650a7792ab4ca6388d21a7c447a1808219
yanik-porto <y.porto@live.com>
2020-11-05 10:42:15 +0100

    combine reducers and persist data

--- Store/configureStore.js
+++ Store/configureStore.js
@@ -1,6 +1,14 @@
 // Store/configureStore.js
 
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import toggleFavorite from './Reducers/favoriteReducer'
+import setAvatar from './Reducers/avatarReducer'
+import { persistCombineReducers } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-export default createStore(toggleFavorite)
+const rootPersistConfig = {
+  key: 'root',
+  storage: storage
+}
+
+export default createStore(persistCombineReducers(rootPersistConfig, {toggleFavorite, setAvatar}))
